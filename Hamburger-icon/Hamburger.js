377dbe6a1138f43c553e0c8613458837e254eebe
taqnar <muzammilthottathil.official@gmail.com>
2020-11-23 10:03:21 +0530

    added hamburger

---
+++ Hamburger-icon/Hamburger.js
@@ -0,0 +1,19 @@
+import styles from "./styles/hamburger.module.scss";
+
+export default function Hamburger({ hamburger, setHamburger }) {
+  return (
+    <div
+      className={
+        hamburger ? `${styles.hamburger} ${styles.open}` : styles.hamburger
+      }
+      onClick={() => setHamburger((ham) => !ham)}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
---
+++ Hamburger-icon/README.md
@@ -0,0 +1,12 @@
+# Hamburger Icon
+
+Demo
+![hamburger](./screenshots/hamburger.gif)
+
+# How to use
+
+Create this component in the project.
+
+Call where you need it,
+
+Say, we need in <Header> Component
